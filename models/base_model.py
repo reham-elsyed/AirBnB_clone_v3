2fa634bec584b1807c41046c178885e3527eb54a
reham elsayed shipl <139706852+reham-elsyed@users.noreply.github.com>
2024-05-24 18:59:10 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+time = "%Y-%m-%d %H:%M:%S.%f"
 
 if models.storage_t == "db":
     Base = declarative_base()
@@ -33,18 +33,18 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
+                self.created_at = datetime.datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.datetime.utcnow()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+                self.updated_at = datetime.datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.datetime.utcnow()
             self.updated_at = self.created_at
 
     def __str__(self):
